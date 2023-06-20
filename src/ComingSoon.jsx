78a3a17f8caa446a7a8cbdc3ea7d9d5b07009174
Dhiraj Wakchaure <ddw147@gmail.com>
2023-06-20 13:09:59 +0530

    COming Soon Page

---
+++ src/ComingSoon.jsx
@@ -0,0 +1,26 @@
+export default function ComingSoon(){
+    const cssStyle = {
+        "background-image": "url('https://vojislavd.com/ta-template-demo/assets/img/coming-soon.jpg')"
+    }
+    return (
+   
+<div class="w-full h-screen" style={cssStyle}>
+    <div class="w-full h-screen flex flex-col items-center justify-between bg-black bg-opacity-70 py-8">
+        <div class="flex-1 flex flex-col items-center justify-center">
+ 
+            <h1 class="text-6xl lg:text-7xl xl:text-8xl text-gray-200 tracking-wider font-bold font-serif mt-12 text-center">Coming Soon</h1>
+            <div class="flex flex-col items-center space-y-4 mt-24">
+                 
+            </div>
+        </div>
+        <div class="w-full flex items-center justify-center">
+            <ul class="flex items-center space-x-4">
+                 
+            </ul>
+        </div>
+    </div>
+</div>
+    )
+
+
+}
